9b2c8842b818702c874efb06cd6054b8b4d7adab
unknown <andre@.lca.net>
2015-04-28 16:30:39 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		
+		<script type="text/javascript" src="lib/jquery-2.1.3/jquery-2.1.3.min.js"></script>
+		
+		<script type="text/javascript" src="lib/bootstrap-3.3.4/dist/js/bootstrap.min.js"></script>
+		
+		<link rel="stylesheet/less" type="text/css" href="style/less/main.less" />
+		
+		
+		<script type="text/javascript" src="lib/less-2.5.0/less.min.js"></script>
+		
+		<title>Página do aluno</title>
+	</head>
+
+	<body>
+		<div id="wrapPage">
+			<div id="accessibilityBar" class="container">
+				<ul class="pull-left">
+					<li>
+						<span>A</span>
+						Ir para acessibilidade
+					</li>
+					<li>
+						<span>K</span>
+						Ir para conteúdo
+					</li>
+					<li>
+						<span>Q</span>
+						Ir para navegação
+					</li>
+					<li>
+						<span>J</span>
+						Tutorial do sistema
+					</li>
+				</ul>
+				<ul class="pull-right">
+					<li>
+						<span>X</span>
+					</li>
+					<li>
+						<span>X</span>
+					</li>
+					<li>
+						<span>X</span>
+					</li>
+					<li>
+						<span>X</span>
+					</li>
+				</ul>
+			</div>
+		</div>		
+
+
+	</body>
+
+</html>
