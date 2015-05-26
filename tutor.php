85fc43ea149d470871b440d9e4b8a8f88906f87f
Andre Luis <andreluisbt@gmail.com>
2015-05-26 11:12:54 -0300

    Tela do tutor

---
+++ tutor.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php include_once 'components/head.php';?>
+		<script type="text/javascript" src="js/dashboard.js"></script>
+		
+		<title>Página do aluno</title>
+	</head>
+
+	<body id="tutor">
+		<div id="wrapPage">
+			
+			<header>
+				<?php include_once 'components/header.php';?>
+			</header>
+			
+			<section id="main" class="container">
+	            
+	            <h4 class="page-title">TURMAS</h4>
+	            
+	            <br/>
+	            <br/>
+	            
+	            <div class="col-md-6">
+    	            <div class="block-title">
+                       <div class="icon">
+                           <i class="fa fa-terminal"></i>
+                       </div>
+                       <div class="title">
+                           Lógica - Introdução a Lógica de Programação
+                       </div>
+                    </div>
+                    
+                    
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            TURMA III
+                        </div>
+                        <div class="panel-body">
+                            <ul class="oas">
+                                <li class="col-xs-6 col-sm-4 col-md-3">
+                                    <a href="#">
+										<i class="fa fa-clipboard"></i>
+										FÓRUM
+									</a>
+                                </li>
+                                <li class="col-xs-6 col-sm-4 col-md-3">
+                                    <a href="#">
+										<i class="fa fa-files-o"></i>
+										OFICINA
+									</a>
+                                </li>
+                                <li class="col-xs-6 col-sm-4 col-md-3">
+                                    <a href="#">
+										<i class="fa fa-check-square-o"></i>
+										NOTAS
+									</a>
+                                </li>
+                                <li class="col-xs-6 col-sm-4 col-md-3">
+                                    <a href="#">
+										<i class="fa fa-comments"></i>
+										CHAT
+									</a>
+                                </li>
+                                 <li class="col-xs-6 col-sm-4 col-md-3">
+                                    <a href="#">
+										<span class="badge">4</span>
+										<i class="fa fa-envelope"></i>
+										MENSAGENS
+									</a>
+                                </li>
+                                 <li class="col-xs-6 col-sm-4 col-md-3">
+                                    <a href="#">
+										<i class="fa fa-info-circle"></i>
+										DÚVIDAS
+									</a>
+                                </li>
+                                <li class="col-xs-6 col-sm-4 col-md-3">
+                                    <a href="#">
+										<i class="fa fa-video-camera"></i>
+										VÍDEO
+									</a>
+                                </li>
+                                <li class="col-xs-6 col-sm-4 col-md-3">
+                                    <a href="#">
+										<i class="fa fa-calendar"></i>
+										CALENDÁRIO
+									</a>
+                                </li>
+                                <li class="col-xs-6 col-sm-4 col-md-3">
+                                    <a href="#">
+										<i class="fa fa-user"></i>
+										USUÁRIOS
+									</a>
+                                </li>
+                                <li class="col-xs-6 col-sm-4 col-md-3">
+                                    <a href="#">
+										<i class="fa fa-gear"></i>
+										CONFIG.
+									</a>
+                                </li>
+                                <li class="col-xs-6 col-sm-4 col-md-3">
+                                    <a href="#">
+										<i class="fa fa-list-ul"></i>
+										FREQUÊNCIA
+									</a>
+                                </li>
+                            </ul>    
+                        </div>
+                    </div>
+                    
+                </div>
+                <div class="col-md-6">
+                	<div class="block-title">
+                       <div class="icon">
+                           <i class="fa fa-terminal"></i>
+                       </div>
+                       <div class="title">
+                           Oracle
+                       </div>
+                    </div>
+                    
+                    
+                	<div class="panel panel-primary">
+                        <div class="panel-heading">
+                            TURMA VI
+                        </div>
+                        <div class="panel-body">
+                            <ul class="oas">
+                                <li class="col-xs-6 col-sm-4 col-md-3">
+                                    <a href="#">
+										<i class="fa fa-clipboard"></i>
+										FÓRUM
+									</a>
+                                </li>
+                                <li class="col-xs-6 col-sm-4 col-md-3">
+                                    <a href="#">
+										<i class="fa fa-files-o"></i>
+										OFICINA
+									</a>
+                                </li>
+                                <li class="col-xs-6 col-sm-4 col-md-3">
+                                    <a href="#">
+										<i class="fa fa-check-square-o"></i>
+										NOTAS
+									</a>
+                                </li>
+                                <li class="col-xs-6 col-sm-4 col-md-3">
+                                    <a href="#">
+										<i class="fa fa-comments"></i>
+										CHAT
+									</a>
+                                </li>
+                                 <li class="col-xs-6 col-sm-4 col-md-3">
+                                    <a href="#">
+										<span class="badge">4</span>
+										<i class="fa fa-envelope"></i>
+										MENSAGENS
+									</a>
+                                </li>
+                                 <li class="col-xs-6 col-sm-4 col-md-3">
+                                    <a href="#">
+										<i class="fa fa-info-circle"></i>
+										DÚVIDAS
+									</a>
+                                </li>
+                                <li class="col-xs-6 col-sm-4 col-md-3">
+                                    <a href="#">
+										<i class="fa fa-video-camera"></i>
+										VÍDEO
+									</a>
+                                </li>
+                                <li class="col-xs-6 col-sm-4 col-md-3">
+                                    <a href="#">
+										<i class="fa fa-calendar"></i>
+										CALENDÁRIO
+									</a>
+                                </li>
+                                <li class="col-xs-6 col-sm-4 col-md-3">
+                                    <a href="#">
+										<i class="fa fa-user"></i>
+										USUÁRIOS
+									</a>
+                                </li>
+                                <li class="col-xs-6 col-sm-4 col-md-3">
+                                    <a href="#">
+										<i class="fa fa-gear"></i>
+										CONFIGURAÇÕES
+									</a>
+                                </li>
+                                <li class="col-xs-6 col-sm-4 col-md-3">
+                                    <a href="#">
+										<i class="fa fa-list-ul"></i>
+										FREQUÊNCIA
+									</a>
+                                </li>
+                            </ul>    
+                        </div>
+                    </div>
+                	
+                </div>
+                
+            </section>
+            
+            <?php include_once 'components/products.php';?>
+		
+			<footer>
+				<?php include_once 'components/footer.php';?>
+			</footer>
+			
+		</div>		
+
+	</body>
+</html>
